d7c49ab81765c71c891b16c2b179fd4e16354226
rakuda04 <abdulkaderbaghaffar@gmail.com>
2023-09-30 12:15:24 +0300

    Created boiler plate for Calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function addition(number1, number2) {
+
+}
+
+function subtraction(number1, number2){
+    
+}
+
+function multiplication(number1, number2){
+    
+}
+
+function division(number1, number2){
+    
+}
+
+function operate(number1,operate,number2){
+
+}
